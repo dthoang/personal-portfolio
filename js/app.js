f5a0cc90a887a1e684e0e5698705b65c1f2b09d5
dthoang <dthoang@uw.edu>
2015-11-19 03:57:56 -0800

    Work in progress, begin to import and display data

--- js/app.js
+++ js/app.js
@@ -26,23 +26,29 @@ myApp.config(function($stateProvider){
 		})
 })
 
+.controller('myCtrl', function($scope, $http){
+	$http.get('json/portfolio_webdev.json').success(function(response){
+		$scope.myData = response
+	})
+})
+
 // Landing page controller: define $scope.number as a number
 .controller('HomeController', function($scope){
 	$scope.introduction = "Hi, I'm Daniel Hoang! Pleast to meet you." //REMOVE THIS LATER
 	$scope.mood = "alright" //this too
 })
 
-// About page controller: define $scope.about as a string
+// Web work controller: define $scope.about as a string
 .controller('WebController', function($scope){
 	$scope.testing = "Import web stuff here."
 })
 
-// Content controller: define $scope.url as an image
+// Artwork controller: define $scope.url as an image
 .controller('ArtController', function($scope){
 	$scope.url = "https://tafa7alkayl.files.wordpress.com/2015/01/love-banana-minions.jpg"
 })
 
-// Content controller: define $scope.url as an image
+// Contact controller: define $scope.url as an image
 .controller('ContactController', function($scope){
 	$scope.url = "http://assets22.pokemon.com/assets/cms2/img/pokedex/full/001.png"
 })
