eebf277618c56074071ae4e6982161f001939c10
dthoang <dthoang@uw.edu>
2015-11-20 03:47:13 -0800

    All data is properly read, some styling added, added navigation bar, still working on how data is displayed/interacted with

--- js/app.js
+++ js/app.js
@@ -7,49 +7,62 @@ myApp.config(function($stateProvider){
 		.state('home', {
 		url: '/home',
 		templateUrl: 'templates/home.html',
-		controller: 'HomeController',
+		controller: 'homeCtrl',
 		})
 		.state('webwork', {
 			url: '/webwork',
 			templateUrl: 'templates/webwork.html',
-			controller: 'WebController'
+			controller: 'webCtrl'
 		})
 		.state('artwork', {
 			url: '/artwork',
 			templateUrl: 'templates/artwork.html',
-			controller: 'ArtController'
+			controller: 'artCtrl'
 		})
 		.state('contact', {
 			url: '/contact',
 			templateUrl: 'templates/contact.html',
-			controller: 'ContactController'
+			controller: 'contactCtrl'
 		})
 })
 
-.controller('myCtrl', function($scope, $http){
-	$http.get('json/portfolio_webdev.json').success(function(response){
-		$scope.myData = response
-	})
-})
+// .controller('webCtrl', function($scope, $http){
+// 	$http.get('json/portfolio_webdev.json').success(function(response){
+// 		$scope.webData = response
+// 	})
 
-// Landing page controller: define $scope.number as a number
-.controller('HomeController', function($scope){
-	$scope.introduction = "Hi, I'm Daniel Hoang! Pleast to meet you." //REMOVE THIS LATER
+// })
+
+// .controller('artCtrl', function($scope, $http){
+// 	$http.get('json/portfolio_webdev.json').success(function(response){
+// 		$scope.webData = response
+// 	})
+
+// })
+
+
+// Home page controller--handles data for the home page
+.controller('homeCtrl', function($scope){
+	$scope.introduction = "Hi, I'm Daniel Hoang! Please to meet you." //REMOVE THIS LATER
 	$scope.mood = "alright" //this too
 })
 
-// Web work controller: define $scope.about as a string
-.controller('WebController', function($scope){
-	$scope.testing = "Import web stuff here."
+// Web portfolio controller--handles data for the web portfolio page
+.controller('webCtrl', function($scope, $http){
+	$http.get('json/portfolio_webdev.json').success(function(response){
+		$scope.webData = response
+	})
 })
 
-// Artwork controller: define $scope.url as an image
-.controller('ArtController', function($scope){
-	$scope.url = "https://tafa7alkayl.files.wordpress.com/2015/01/love-banana-minions.jpg"
+// Artwork page controller--handles data for the art portfolio page
+.controller('artCtrl', function($scope, $http){
+	$http.get('json/portfolio_artwork.json').success(function(response){
+		$scope.artData = response
+	})
 })
 
-// Contact controller: define $scope.url as an image
-.controller('ContactController', function($scope){
-	$scope.url = "http://assets22.pokemon.com/assets/cms2/img/pokedex/full/001.png"
+// Contact page controller--handles data for the contact info page
+.controller('contactCtrl', function($scope){
+	$scope.greeting = "hello!"
 })
 
